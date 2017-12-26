045f118c4e7321d5b3d5a96922316cc953de0111
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-25 22:30:15 -0800

    debug rule in makefile working

---
+++ Makefile
@@ -0,0 +1,35 @@
+CC = gcc
+NAME = fillit
+SRCS = fillit.c
+OBJS = $(patsubst %.c, %.o, $(SRCS))
+LIBD = -L./libft -lft
+CFLAGS = -Wall -Werror -Wextra
+OFLAGS = $(CFLAGS) $(LIBD)
+DSRCS = $(SRCS)
+DFLAGS = $(CFLAGS) -g $(LIBD) $(DSRCS) -o
+DNAME = $(NAME)_debug
+DOBJS = $(patsubst %.c, %.o, $(DSRCS))
+
+all: $(NAME)
+	
+$(NAME): $(OBJS)
+	$(CC) $(OFLAGS) $(NAME) $(OBJS)
+
+$(OBJS): $(SRCS)
+	$(CC) $(CFLAGS) -c $(SRCS)
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+	rm -f $(DNAME)
+	rm -rf $(DNAME).dSYM
+re: fclean
+	make
+
+debug: $(DNAME)
+
+$(DNAME):
+	$(CC) $(DFLAGS) $(DNAME)
+	lldb $(DNAME) $(DARGS)
--- fillit.c
+++ fillit.c
@@ -6,32 +6,54 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/23 16:04:46 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/25 18:37:30 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/25 22:27:47 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
-int		main(int argc, char **argv)
+static	char *read_tetronimo_set(char *file)
 {
 	int		file_descriptor;
 	int		bytes_read;
 	char	file_buffer[TETRA_BYTES + 1];
-	char 	*tetronimo;
-
-	tetronimo = NULL;
-	if (argc != 2)
+	char 	*unvalidated_tetromino_set;
+	char	*temporary_tetromino;
+	
+	unvalidated_tetromino_set = NULL;
+	file_descriptor = open(file, O_RDONLY);
+	if(!file_descriptor)
 	{
-		return(-1);
+		return(NULL);
 	}
 
-	if(!(file_descriptor = open(argv[1], O_RDONLY)))
+	bytes_read = TETRA_BYTES;
+	while(bytes_read == TETRA_BYTES)
 	{
-		return(0);
+		bytes_read = read(file_descriptor, file_buffer, TETRA_BYTES);
+		file_buffer[bytes_read] = '\0';
+		temporary_tetromino = (char *)malloc(sizeof(char) * ft_strlen(file_buffer) + 1);
+		ft_memmove(unvalidated_tetromino_set, file_buffer, bytes_read);
+		free(file_buffer);
+		unvalidated_tetromino_set += bytes_read;	
 	}
-	bytes_read = read(file_descriptor, file_buffer, TETRA_BYTES);
-	file_buffer[bytes_read + 1] = '\0';
-	tetronimo = (char *)malloc(sizeof(char) * ft_strlen(file_buffer) + 1);
-	ft_memmove(tetronimo, file_buffer, TETRA_BYTES + 1);
-	ft_putstr(tetronimo);
+	return (unvalidated_tetromino_set);
+}
+
+int		main(int argc, char **argv)
+{
+	char *unvalidated_tetromino_set;
+
+	if(argc != 2)
+	{
+		return (0);
+	}
+	unvalidated_tetromino_set = read_tetronimo_set(argv[1]);
+/*
+	valid_tetromino_set = validate_tetromino_set(unvalidated_tetromino_set);
+	if(!valid_tetromino_set)
+	{
+		return(NULL);
+	}
+*/	
+	ft_putstr(unvalidated_tetromino_set);
 }
