a9eceb3424b84635085ec86517e1f77af30820d7
Poitier Stringer <pstringe@e1z3r9p23.42.us.org>
2017-12-26 10:56:31 -0800

    fixed bug in read function

--- fillit.c
+++ fillit.c
@@ -6,36 +6,38 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/23 16:04:46 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/25 22:27:47 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/26 10:17:45 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 static	char *read_tetronimo_set(char *file)
 {
+	int		file_size;
 	int		file_descriptor;
 	int		bytes_read;
 	char	file_buffer[TETRA_BYTES + 1];
 	char 	*unvalidated_tetromino_set;
-	char	*temporary_tetromino;
 	
-	unvalidated_tetromino_set = NULL;
+	unvalidated_tetromino_set = (char *)malloc(sizeof(char) * 21 * 26 + 1);
 	file_descriptor = open(file, O_RDONLY);
 	if(!file_descriptor)
 	{
 		return(NULL);
 	}
-
+	
+	file_size = 0;
 	bytes_read = TETRA_BYTES;
 	while(bytes_read == TETRA_BYTES)
 	{
 		bytes_read = read(file_descriptor, file_buffer, TETRA_BYTES);
+		file_size += bytes_read;
 		file_buffer[bytes_read] = '\0';
-		temporary_tetromino = (char *)malloc(sizeof(char) * ft_strlen(file_buffer) + 1);
 		ft_memmove(unvalidated_tetromino_set, file_buffer, bytes_read);
-		free(file_buffer);
 		unvalidated_tetromino_set += bytes_read;	
 	}
+	*unvalidated_tetromino_set = '\0';
+	unvalidated_tetromino_set -= file_size;
 	return (unvalidated_tetromino_set);
 }
 
