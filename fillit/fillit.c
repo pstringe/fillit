c29d1da6a405b0621f0c93e7743b8cffd0d30ef8
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-29 17:08:12 -0800

    fixed 1d to 2d function

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,13 +6,13 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/23 16:04:46 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/28 15:01:17 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/29 16:33:09 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int		has_valid_number_of_connections(char *tetromino)
+static int		has_valid_number_of_monominos(char *tetromino)
 {
 	if(!tetromino)
 	{
@@ -21,13 +21,86 @@ static int		has_valid_number_of_connections(char *tetromino)
 	return(1);
 }
 
-static int		has_valid_number_of_monominos(char *tetromino)
+static int		is_neighbor(char *tetromino, int i)
 {
+	if (i >= 0 && i < 16 && tetromino[i] != '.')
+	{
+		return (1);
+	}
+	return (0);
+}
+static int		get_number_of_neighbors(char *tetromino, int i)
+{
+	int *coords;
+	int neighbors;
+	int x;
+	int y;
+	int left;
+	int right;
+	int up;
+	int down;
+
+	coords = access_second_dimension(4, i);
+	x = coords[1];
+	y = coords[2];
+
+	right = access_first_dimension(4, y, x - 1);
+	left = access_first_dimension(4, y, x + 1);
+	up = access_first_dimension(4, y - 1, x);
+	down = access_first_dimension(4, y + 1, x);
+	neighbors = 0;
+	/*check surrounding indecies right*/
+	if (is_neighbor(tetromino, right))
+	{
+		neighbors++;
+	}
+	else if (is_neighbor(tetromino, left))
+	{
+		neighbors++;
+	}
+	else if (is_neighbor(tetromino, up))
+	{
+		neighbors++;
+	}
+	else if (is_neighbor(tetromino, down))
+	{
+		neighbors++;
+	}
+	return (neighbors);
+}
+
+static int		has_valid_number_of_connections(char *tetromino)
+{
+	int i;
+	int neighbors;
+
 	if(!tetromino)
 	{
 		return (0);
-	}	
-	return (1);
+	}
+	i = -1;
+	neighbors = 0;
+	while (tetromino[++i])
+	{
+		if(tetromino[i] == '.')
+		{
+			i++;
+		}
+		else if(neighbors >= 6)
+		{
+			return (1);
+		}
+		else
+		{
+			neighbors += get_number_of_neighbors(tetromino, i);
+		}
+		i++;
+	}
+	if (neighbors >= 6)
+	{
+		return(1);
+	}
+	return (error(-9));
 }
 
 static int		is_valid_tetromino (char *tetromino)
@@ -36,7 +109,7 @@ static int		is_valid_tetromino (char *tetromino)
 	{
 		return (0);
 	}
-	if(has_valid_number_of_monominos(tetromino) && has_valid_number_of_connections(tetromino))
+	if(has_valid_number_of_monominos(tetromino) && has_valid_number_of_connections(tetromino) > 0)
 	{
 		return (1);
 	}
@@ -52,7 +125,7 @@ char	**validate_tetrominos(char **tetrominos)
 	{
 		if(!is_valid_tetromino(tetrominos[i]))
 		{
-			ft_two_d_free((void **)tetrominos);
+			return(NULL);
 		}
         i++;
 	}
