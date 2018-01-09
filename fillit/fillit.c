a6c581a0191a9d39da1816f01b77f62d71f409f4
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2018-01-09 14:16:29 -0800

    moved read and validate functions

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -12,37 +12,86 @@
 
 #include "fillit.h"
 
-typedef struct	square_grid
-{
-	int		side;
-	char	**array;
-}				grid;
-
-typedef struct	grid_location
+typedef struct	s_grid_location
 {
 	int						x;
 	int						y;
-
 	int						value;
 	int						parent;
-	
 	struct grid_location	*prev;
 	struct grid_location	*next;
-
 	struct grid_location	*up;
 	struct grid_location	*down;
 	struct grid_location	*right;
 	struct grid_location	*left;
 }				point;
 
-typedef struct	tetromino
+typedef struct	s_tetromino
 {
-	int		order;
-	int		value;
-	struct grid_location *origin;
-	struct grid_location *second;
-	struct grid_location *third;
-}				tet;
+	int						order;
+	int						value;
+	struct grid_location	*origin;
+	struct grid_location	*second;
+	struct grid_location	*third;
+	struct grid_location	*fourth;
+}				t_tet;
+
+typedef struct	s_board
+{
+	int						validity;
+	int						size;
+	unsigned short int		*tets;
+	struct grid_location	*origin;
+}				t_board;
+
+t_tet	*inititalize_spatial_tet(unsigned short int code, int order)
+{
+	t_tet	*tet;
+	tet = malloc(sizeof(t_tet*));
+	tet->order = order;
+	tet->value = code;
+	tet->origin = NULL;
+	tet->second = NULL;
+	tet->third	= NULL;
+	tet->fourth = NULL:
+	return (tet)
+}
+
+t_tet	**prepare_for_placement(unsigned short int tet_codes)
+{
+	t_tet **spatial_tets;
+	
+	int	i;
+
+	i = 0;
+	while(tet_codes)
+	{
+		i++;
+	}
+	spatial_tets = malloc(sizeof(t_tet*) * i + 1);
+	i = -1;
+	while(tet_codes[++i])
+	{
+		*(spatial_tets + i) = initialize_spatial_tet(tet_codes[1], i);
+	}
+	spatial_tets[i] = NULL;
+	return (spatial_tets);
+}
+
+int		solve(unsigned short int tet_codes)
+{
+	t_tet		**tets;
+	s_board 	*board;
+	
+	tets = prepare_for_placement(tet_codes);
+	while(tets)
+	{
+		print_encoded_tetromino(*tets);
+		tets++;
+	}
+	//generate_board(tets, size);
+	print_board(board);
+}
 int		main(int argc, char **argv)
 {
 	unsigned short int	*tet_codes;
@@ -52,6 +101,7 @@ int		main(int argc, char **argv)
 		ft_putendl("Your shit sucks!");
 		return (-1);
 	}
+	solve(tet_codes);
 	ft_putendl("It works");
 	return (0);
 }
