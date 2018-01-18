93e464c4e0b3ff390f88510ebe6b4b5e7890b932
Poitier Stringer <pstringe@e1z2r9p28.42.us.org>
2018-01-18 13:48:58 -0800

    tetromino placement works

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,17 +6,113 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/23 16:04:46 by pstringe          #+#    #+#             */
-/*   Updated: 2018/01/02 17:43:43 by ralee            ###   ########.fr       */
+/*   Updated: 2018/01/18 13:47:22 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+int		int_sqrt(int y, int x)
+{
+	if ( y * y >= x)
+	{
+		return (y);
+	}
+	else
+	{
+		return (int_sqrt(++y, x));
+	}
+}
+
+int		calc_minsize(int n)
+{
+	if(n != 1)
+	{
+		return (int_sqrt(0, n * 4));
+	}
+	return (2);
+}
+
+int		tet_no(char **tets)
+{
+	int n;
+
+	n = 0;
+	while (*(tets + n))
+	{
+		n++;
+	}
+	return (n);
+}
+
+char 	**generate_board(int no_of_tets, int exp)
+{
+	int min_size = calc_minsize(no_of_tets);
+	char **board;
+	int i;
+	int j;
+
+	board = malloc(sizeof(char*) * min_size + 1);
+	i = 0;
+	while (i <= min_size + exp)
+	{
+		j = 0;
+		board[i] = malloc(min_size + 1);
+		while (j <= min_size + exp)
+		{
+			board[i][j] = '.';
+			j++;
+		}
+		board[i][j] = '\0';
+		i++;
+	}
+	board[i] = NULL;
+
+	return(board);
+}
+
+void	print_board(char **board)
+{
+	while (*board)
+	{
+		ft_putendl(*board);
+		board++;
+	}
+}
+
+char	**place_tet(char **board, char *tet, int x, int y)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (board[x + i][y + j] == '.')
+			{
+				board[x + i][y + j] = tet[access_first_dimension(4, i, j)];
+				j++;
+			}
+			else
+			{
+				return (NULL);
+			}
+		}
+		i++;
+	} 
+	return (board);
+}
+
 int		main(int argc, char **argv)
 {
+	int 	no_of_tets;
 	char 	*unvalidated_tetromino_set;
 	char	*valid_tetromino_set;
 	char	**tetrominos;
+	char	**board;
 
 	if(argc != 2)
 	{
@@ -33,10 +129,10 @@ int		main(int argc, char **argv)
 	tetrominos = validate_tetrominos(tetrominos);
 	// normalize_tetrominos moves all tetrominos to top left corner
 	normalize_tetrominos(tetrominos);
-	if(!tetrominos)
-	{
-		return(error(-8));
-	}
-	ft_putendl("It works");
+	no_of_tets = tet_no(tetrominos);
+	board = generate_board(no_of_tets, 0);
+	print_board(board);
+	board = place_tet(board, *tetrominos, 0, 0);
+	print_board(board);
 	return (0);
 }
