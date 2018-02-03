a439a9c15b8f6cac64c9298d179b5c916a415ae7
Poitier Stringer <pstringe@e1z2r9p9.42.us.org>
2018-02-03 11:18:55 -0800

    added comments to fillit.c

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,11 +6,15 @@
 /*   By: ralee <ralee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/23 16:04:46 by pstringe          #+#    #+#             */
-/*   Updated: 2018/01/29 10:41:36 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/02/03 11:17:41 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+/*
+ *	estimates the squar root of a number for use in board generation 
+ */
 int		int_sqrt(int y, int x)
 {
 	if ( y * y >= x)
@@ -22,6 +26,10 @@ int		int_sqrt(int y, int x)
 		return (int_sqrt(++y, x));
 	}
 }
+
+/*
+ * calculates the minimim size board given the number of tets
+ */
 int		calc_minsize(int n)
 {
 	if(n != 1)
@@ -31,7 +39,13 @@ int		calc_minsize(int n)
 	return (2);
 }
 
-
+/*
+ * takes a set of tetromnios and generates the root, this function currently does not account for expansion, 
+ * In the past I've used an extra integer input to expand the board, 
+ * but I kept running into strange memory issues. 
+ * I need to follow the proccess of board generation carefully on paper, then write the function,
+ * then follow it in the debugger.
+ */
 t_board		*generate_board(int no_of_tets, t_et *tets)
 {
 	t_board	*board_struct;	
@@ -64,6 +78,9 @@ t_board		*generate_board(int no_of_tets, t_et *tets)
 	return(board_struct);
 }
 
+/*
+ * displays the boards map to standard in
+ */
 void	print_board(char **board)
 {
 	while (*board)
@@ -73,6 +90,9 @@ void	print_board(char **board)
 	}
 }
 
+/*
+ * pops a tet from the stack 
+ */
 t_et	*get_next_tet(t_et *tets)
 {
 	t_et	*tmp;
@@ -86,6 +106,11 @@ t_et	*get_next_tet(t_et *tets)
 	return (tmp);
 }
 
+/*
+ * was using this to execute a clearing condition when place_tet and try_tet were the same function
+ * now I'm not using it since there are only placements after its been determined there is space.
+ * this function  may still come in handy when the program backtracks.
+ */
 void	clear_board(t_board *board)
 {
 	int i;
@@ -105,6 +130,9 @@ void	clear_board(t_board *board)
 	}
 }
 
+/*
+ *	checks to see if there is available space for the current tet	 	
+ */
 int			try_tet(t_board *board, int x, int y)
 {
 	int 	i;
@@ -129,6 +157,10 @@ int			try_tet(t_board *board, int x, int y)
 	return (1);
 }
 
+/*
+ * this function places a tet on the board at a specified index, it is only called if the try_tet function 
+ * returns true, indicating there is space available
+ */
 t_board		*place_tet(t_board *board, int x, int y)
 {
 	int i;
@@ -164,7 +196,11 @@ t_board		*place_tet(t_board *board, int x, int y)
 	tet->placed = 1;
 	return (board);
 }
-t_board		*place_the_next_motherfucking_tet(t_board *board)
+
+/*
+**	this function attempt to place the next tet on the stack onto the boards map, by first seeing if there are **  any available spaces, and then, if space is available, places the tet.
+*/
+t_board		*place_the_next_tet(t_board *board)
 {
 	int 	x;
 	int 	y;
@@ -195,9 +231,13 @@ t_board		*place_the_next_motherfucking_tet(t_board *board)
 	return (NULL);
 }
 
+/*
+** to become the main solving function that will expand the board if the sub_solver returns no solution,
+** right now, I'm using in to test the functions I have so far.	
+*/
 t_board		*solve(t_board *board)
 {	
-	if (place_the_next_motherfucking_tet(board))
+	if (place_the_next_tet(board))
 	{
 		print_board(board->map);
 	}
