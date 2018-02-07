9bcf3b11d531ac32ff14dbe400d7593646318ec3
Poitier Stringer <pstringe@e1z2r9p9.42.us.org>
2018-02-07 15:32:54 -0800

    the new read loop works, now to test the new validation functions

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,16 +6,16 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 16:38:40 by pstringe          #+#    #+#             */
-/*   Updated: 2018/02/06 21:15:14 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/02/07 15:30:34 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #define	PIECE	21
-
+#define	TET		16
 /*
  *	counts a monomino's adjacent monominos
- */
+
 int			adj(char *str, int i)
 {
 	int		top;
@@ -31,9 +31,9 @@ int			adj(char *str, int i)
 	return ((top == '#') + (bottom == '#') + (right == '#') + (left == '#'));
 }
 
+ */
 /*
  * validates an individual chunk
- */
 int			validate(char **str)
 {
 	int		i;
@@ -49,7 +49,7 @@ int			validate(char **str)
 	a = 0;
 	count = 0;
 	i = -1;
-	while (++i < PIECE)
+	while (++i < TET)
 	{
 		if (str[i] && !(tet[i] == '.' || tet[i] == '#'))
 			return (0);
@@ -61,9 +61,9 @@ int			validate(char **str)
 	return (count == 4 && (a == 6 || a == 8));
 }
 
+ */
 /*
  * creates a tet and pushes it onto the list
- */
 void		push_tet(t_et **head, char *buf)
 {
 	static int		order;
@@ -85,36 +85,108 @@ void		push_tet(t_et **head, char *buf)
 	tet->prev = NULL;
 	*head = tet; 
 }
+*/
+/*
+ * calculates cieling of sqrt
+int		ft_sqrt(int y, int x)
+{
+	if ( y * y >= x)
+	{
+		return (y);
+	}
+	else
+	{
+		return (ft_sqrt(++y, x));
+	}
+}
 
+
+int			calc_minsize(int n)
+{
+	if(n != 1)
+	{
+		return (ft_sqrt(0, n * 4));
+	}
+	return (2);
+}
+
+ */
+/*
+ * generates a board based on the number of tets and the expansion
+t_board		root(t_et **head, int exp)
+{
+	t_board	*board;	
+	int 	min_size;
+	char	**map;
+	int		i;
+	int		j;
+
+	min_size = calc_minsize(no_of_tets);
+	board = malloc(sizeof(t_board));
+	map = malloc(sizeof(char*) * min_size + 1);
+	i = -1;
+	while (++i < min_size)
+	{
+		j = -1;
+		map[i] = malloc(min_size + 1);
+		while (++j < min_size)
+		{
+			map[i][j] = '.';
+		}
+		map[i][j] = '\0';
+	}
+	map[i] = NULL;
+	board->size = min_size;
+	board->stack = tets;
+	board->map = board;
+	return(board);
+}
+
+ */
 /*
  * reads and validates the file one chunck at a time
  */
-t_board		*read_and_validate(char *file)
+void/*t_board		*/read_and_validate(char *file)
 {
+	int		bytes;
 	int		fd;
 	t_et	**head;
 	char	buf[PIECE + 1];
 
+
 	head = NULL;
 	fd = open(file, O_RDONLY);
-	while(read(fd, buf, PIECE) >= PIECE - 1 && validate(ft_strsplit(buf, '\n')))
+	while((bytes = read(fd, buf, PIECE)) >= PIECE - 1 /*&& validate(ft_strsplit(buf, '\n'))*/)
 	{
-		push_tet(head, buf);
+		if(bytes == PIECE)
+		{
+			buf[PIECE] = '\0';
+		}
+		else if(bytes == PIECE - 1)
+		{
+			buf[PIECE - 1] = '\0';
+		}
+		ft_putstr(buf);	
+		//push_tet(head, buf);
 	}
+	/*
 	while((*head)->prev)
 	{
 		head = &((*head)->prev);
 	}
 	return(root(head, 0));
+	*/
 }
+
 
 int			main(int argc, char **argv)
 {
-	t_board		*board;
+	//t_board		*board;
 
 	if(argc != 2)
 		return (error(1));
-	else if (!(board = read_and_validate(argv[1])))
-		return (error(2));
+	//else if (!(board = read_and_validate(argv[1])))
+	//	return (error(2));
+	read_and_validate(argv[1]);
 	return (0);
 }
