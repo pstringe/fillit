f7c002f2c3bd2f1c8c791369bd2a1a540bcdc1aa
Poitier Stringer <pstringe@e1z2r9p9.42.us.org>
2018-02-14 20:15:16 -0800

    solve functions need tweaking

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,29 +6,82 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 16:38:40 by pstringe          #+#    #+#             */
-/*   Updated: 2018/02/14 19:30:08 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/02/14 20:11:59 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-int		resolve(t_board *board)
+
+t_board		*place_tet(t_board *board, t_et *tet, int x, int y)
 {
-	print_map(board);
-	if(board->exp < 3)
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i + y <= board->size - tet->bound_y)
 	{
-		return (0);
+		j = -1;
+		while (++j + x <= board->size - tet->bound_x)
+		{
+			board->map[i + y][j + x] = tet->label;
+		}
+	}
+	return (board);
+}
+
+int			try_tet(t_board *board, t_et *tet, int x, int y)
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i <= board->size - tet->bound_y)
+	{
+		j = -1;
+		while (++j < board->size - tet->bound_x)
+		{
+			if ((board->map)[y + i][x + j] != '.' &&
+				(tet->value)[(i * 3) + j] != '.')
+			{
+				return(0);
+			}
+		}
 	}
 	return (1);
 }
 
-t_board	*solve(t_board *board)
+t_board		*resolve(t_board *board, t_list *lst)
 {
-	t_list	*tmp;
-	
-	tmp = *(board->stack);
-	while (!resolve(board))
+	int		x;
+	int		y;
+	t_et	*tet;
+
+	tet = (t_et*)(lst->content); 
+	if (!tet)
 	{
-		board = root(tmp, board->exp + 1);
+		return (board);
+	}
+	y = -1;
+	while (++y <=  board->size - tet->bound_y)
+	{
+		x = -1;
+		while (++x <= board->size - tet->bound_x)
+		{
+			if (try_tet(board, tet, x, y))
+			{
+				place_tet(board, tet, x, y);
+				return(resolve(board, lst->next));
+			}
+		}
+	}
+	return (NULL);
+}
+
+t_board		*solve(t_board *board)
+{
+	while (!resolve(board, *(board->stack)))
+	{
+		board = root(*(board->stack), board->exp + 1);
 		ft_putendl("");
 	}
 	return (NULL);
