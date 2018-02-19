f1ffe26c38772eb2e7f1b2af754f89a02a86cd2b
Poitier Stringer <pstringe@e1z2r9p9.42.us.org>
2018-02-19 15:18:19 -0800

    fillit.c normed

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 16:38:40 by pstringe          #+#    #+#             */
-/*   Updated: 2018/02/18 16:40:36 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/02/19 15:17:52 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		main(int argc, char **argv)
 {
 	t_board		*board;
 
-	if(argc != 2)
+	if (argc != 2)
 		return (error(1));
 	if (!(board = read_and_validate(argv[1])))
 		return (error(2));
